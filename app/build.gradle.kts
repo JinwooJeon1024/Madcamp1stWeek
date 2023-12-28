cd6775bd5f3c60246838bb403a23bbf2a2a474a8
ssoo174 <itssoo174@gmail.com>
2023-12-28 18:47:32 +0900

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.madcamp1stweek"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.madcamp1stweek"
