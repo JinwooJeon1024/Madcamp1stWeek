5e4a5188fbf1ff78b446b2541edf08a52e10bf72
ssoo174 <itssoo174@gmail.com>
2023-12-29 14:26:33 +0900

    ssoo수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,10 +36,16 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    sourceSets {
+        getByName("main") {
+            assets {
+                srcDirs("src/main/assets")
+            }
+        }
+    }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -51,4 +57,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.code.gson:gson:2.8.6")
 }
