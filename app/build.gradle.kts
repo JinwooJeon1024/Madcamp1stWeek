7feffe50a7a53b6c867f7665d21d8adb1043c6ea
ssoo174 <itssoo174@gmail.com>
2023-12-30 21:44:50 +0900

    탭1 카드로 바꿈

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
